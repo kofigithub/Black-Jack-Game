f943d1eaee187951b438038d948b6a463de0556f
Kofi Owusu-Tufuor <hoocha88@yahoo.com>
2021-11-18 20:29:32 +0000

    Initial commit

--- src/Card_Data.java
+++ src/Card_Data.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 public class Card_Data {
 
-       /* static List<Card> cardlist = Arrays.asList(
+        static List<Card> cardlist = Arrays.asList(
         new Card(1, Suit.HEARTS, "hearts", Card_Value.Queen),
         new Card(2, Suit.DIAMONDS, "diamonds", Card_Value.Two),
         new Card(3, Suit.CLUBS, "clubs", Card_Value.Two),
-        new Card(4, Suit.SPADES, "spades", Card_Value.Two));*/
-
+        new Card(4, Suit.SPADES, "spades", Card_Value.Two));
     public static void getValues(int model_value) {
 
         Card_Value values[] = Card_Value.values();
@@ -20,8 +19,9 @@ public class Card_Data {
 
             Card card = new Card(1,Suit.HEARTS,"hearts",Card_Value.Three);
             ArrayList<Card> cardlist = new ArrayList<>();
-            System.out.println(Card_Value.Three.getValue());
+
              Player player1 = new Player(5, "K",cardlist);
-            System.out.println(player1);
+           // System.out.println(player1);
+            System.out.println(Card_Value.Three.getValue());
         }
 }
